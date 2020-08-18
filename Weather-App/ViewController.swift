4bdaa2ae1caa9279a46d46dbe1022eb260697bec
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-08-18 13:42:13 +0300

    Init

---
+++ Weather-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather-App
+//
+//  Created by rs on 18.08.2020.
+//  Copyright © 2020 Oleksandr Myronovych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
